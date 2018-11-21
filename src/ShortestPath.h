56997e43483e09624a2c1c3d3e418e336875bdc4
Mateo Tutic <mateo@tutic.ch>
2018-11-21 16:13:24 +0100

    Commentaires Dijkstra

--- src/ShortestPath.h
+++ src/ShortestPath.h
@@ -90,7 +90,7 @@ public:
 
       // Itérations jusqu'a ce que la pile soit vide
       while (!pq.empty()) {
-         // On sélectionne le sommet u avec le plus petit distanceTo[]
+         // On sélectionne le sommet u avec la plus petite distanceTo[]
          int u = (*(pq.begin())).second;
 
          // On retire le sommet u de la pq
